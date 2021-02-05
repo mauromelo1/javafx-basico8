926b707c94834cbfceffb3f549d54eb91d6af41e
Mauro Melo <mauromelo1@gmail.com>
2021-02-05 13:24:08 -0300

    Gerenciadores de layout: AnchorPane

---
+++ src/application/AppLayout.java
@@ -0,0 +1,27 @@
+package application;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class AppLayout extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+
+			Scene principal = new Scene(new TesteAnchorPane(), 800, 600);
+			
+			primaryStage.setScene(principal);
+			primaryStage.setTitle("Gerenciadores de Layout");
+			primaryStage.show();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
